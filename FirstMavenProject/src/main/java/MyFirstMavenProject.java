1702f0843d24710e6b870646dd72cd5f48085a30
Stefan Schade <dr_stefan_schade@yahoo.com>
2019-09-15 22:14:58 +0200

    first commit

---
+++ FirstMavenProject/src/main/java/MyFirstMavenProject.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MyFirstMavenProject {
+
+    public static void main(String[] args) {
+         System.out.println(StringUtils.capitalize("this will be capitalized"));
+    }
+}
